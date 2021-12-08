950f2563adfaeb55770605cd4fabf3496ec833ef
codingSlate <ananta2021k@gmail.com>
2021-12-08 17:57:40 +0530

    2 files modified

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
+const initFields ={
+  fname: '',
+  lname: '',
+}
+
 
 const Form = () => {
+  const [form, setForm] = useState(initFields);
+  const onChangeHandler = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  console.log(form);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault;
+    setForm()
+  };
+  
+  const onResetHandler = (e) => {
+
+    // setForm()
+  };
+
+
   return (
     <>
-      <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" />
+      <form onSubmit={onSubmitHandler}>
+        <label htmlFor="fname">First Name</label>
+        <input type="text" id="fname" onChange={onChangeHandler} name="fname" />
+        <label htmlFor="lname">Last Name</label>
+        <input type="text" id="lname" onChange={onChangeHandler} name="lname" />
+
+        <button type="submit">Submit</button>
+        <button type="button" onClick={onResetHandler}>Reset</button>
       </form>
     </>
   );
