752c2618e093a6ef03d6541702bdfd8708dd4881
codingSlate <ananta2021k@gmail.com>
2021-12-08 18:06:14 +0530

    2 files modified

--- src/Form.js
+++ src/Form.js
@@ -1,39 +1,60 @@
 import React, { useState } from 'react';
 import './Form.css';
-const initFields ={
+const initFields = {
   fname: '',
   lname: '',
-}
-
+  msg: '',
+};
 
 const Form = () => {
   const [form, setForm] = useState(initFields);
   const onChangeHandler = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
-  console.log(form);
+
+  // console form
 
   const onSubmitHandler = (e) => {
-    e.preventDefault;
-    setForm()
-  };
-  
-  const onResetHandler = (e) => {
-
-    // setForm()
+    e.preventDefault();
+    console.log(form);
   };
 
+  const onResetHandler = () => {
+    setForm(initFields);
+  };
 
   return (
     <>
       <form onSubmit={onSubmitHandler}>
         <label htmlFor="fname">First Name</label>
-        <input type="text" id="fname" onChange={onChangeHandler} name="fname" />
+        <input
+          type="text"
+          id="fname"
+          onChange={onChangeHandler}
+          name="fname"
+          value={form.fname}
+        />
         <label htmlFor="lname">Last Name</label>
-        <input type="text" id="lname" onChange={onChangeHandler} name="lname" />
+        <input
+          type="text"
+          id="lname"
+          onChange={onChangeHandler}
+          name="lname"
+          value={form.lname}
+        />
+        <label htmlFor="msg">Message</label>
+        <textarea
+          row="10"
+          id="msg"
+          onChange={onChangeHandler}
+          name="msg"
+          value={form.msg}
+        ></textarea>
 
         <button type="submit">Submit</button>
-        <button type="button" onClick={onResetHandler}>Reset</button>
+        <button type="button" onClick={onResetHandler}>
+          Reset
+        </button>
       </form>
     </>
   );
